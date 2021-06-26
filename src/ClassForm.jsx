c3aa8a67e808ea88025aefd59556407bed2f7d6e
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-06-26 18:06:25 +0000

    classform jsx update

--- src/ClassForm.jsx
+++ src/ClassForm.jsx
@@ -1,27 +1,79 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 
-class ClassForm extends Component {
-    render() {
-        return (
-            <div>
-                <form>
-                    <div>
-                        <label>Name</label>
-                        <br />
-                        <input type="text" name="name" value={this.state.name} onChange={this.nameChange}>
-                        </input>
-                    </div>
-                    <div>
-                        <label>description</label>
-                        <br />
-                        <textarea name="description" value={this.state.description} onChange={this.nameChange}>
-                        </textarea>
-                    </div>
-                    <button>Submit</button>
-                </form>
-            </div>
-        );
-    }
-}
+const FunctionForm = (props) => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-export default ClassForm;
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form.name);
+    console.log(form.email);
+    console.log(form.password);
+  };
+
+  return (
+    <div>
+      <div className="form">
+        <h2>Form using Function Component</h2>
+
+        <label htmlFor="name" className="label">
+          Name
+        </label>
+        <br />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={form.name}
+          placeholder="Name"
+          onChange={handleChange}
+        />
+        <br />
+
+        <label htmlFor="Email" className="label">
+          Email
+        </label>
+        <br />
+        <input
+          type="Email"
+          name="email"
+          id="Email"
+          value={form.email}
+          placeholder="Example@gmail.com"
+          onChange={handleChange}
+        />
+        <br />
+
+        <label htmlFor="password" className="label">
+          Password
+        </label>
+        <br />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={form.password}
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <br />
+
+        <button onClick={handleSubmit} type="submit">
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FunctionForm;
